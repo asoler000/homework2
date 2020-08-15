95852c98509ac8c1444d36e47bd78948d3c60ff0
asoler000 <68714690+asoler000@users.noreply.github.com>
2020-08-15 13:16:44 -0400

    Add files via upload
    i think i will finally get the contact me portion of website to work using the free html 5 php forms

--- html5-contact-form-send-email/handler.php
+++ html5-contact-form-send-email/handler.php
@@ -21,6 +21,6 @@ $validator->field('comments')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('someone@gmail.com'); // ← Your email here
+$pp->sendEmailTo('asoler000@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
